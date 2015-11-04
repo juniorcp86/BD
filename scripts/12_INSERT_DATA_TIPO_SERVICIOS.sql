c0ac8e72431b96d18c1f7bf6f0be28eaf3106af3
JuniorCasique <jcasique@teravisiontech.com>
2015-11-04 15:59:43 -0430

    Actualización para agregar más datos.
    Se estan agregando más datos iniciales para la primera versión de
    KIOSCO. Falta agregar información en columnas de las tablas
    actualizadas.

--- scripts/12_INSERT_DATA_TIPO_SERVICIOS.sql
+++ scripts/12_INSERT_DATA_TIPO_SERVICIOS.sql
@@ -10,12 +10,20 @@
 
 USE KIOSCO;
 
-INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Tarjetas telefónicas Movistar','Distribución de tarjetas telefónicas para Movistar.',CURRENT_DATE(),NULL,NULL);
-INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Tarjetas telefónicas Movilnet','Distribución de tarjetas telefónicas para Movilnet.',CURRENT_DATE(),NULL,NULL);
-INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Tarjetas telefónicas Digitel','Distribución de tarjetas telefónicas para Digitel.',CURRENT_DATE(),NULL,NULL);
-INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Tarjetas telefónicas Direct TV','Distribución de tarjetas telefónicas para Direct TV.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Tarjetas telefónicas Movistar','Distribución de tarjetas telefónicas y/o reacarga para Movistar.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Tarjetas telefónicas Movilnet','Distribución de tarjetas telefónicas y/o reacarga para Movilnet.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Tarjetas telefónicas Digitel','Distribución de tarjetas telefónicas y/o recarga para Digitel.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Tarjetas Direct TV','Distribución de tarjetas y/o recarga para Direct TV.',CURRENT_DATE(),NULL,NULL);
 INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Tarjetas telefónicas llamadas internacionales','Distribución de tarjetas telefónicas para llamadas internacionales.',CURRENT_DATE(),NULL,NULL);
 INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Revistas nacionales','Distribución de revistas nacionales.',CURRENT_DATE(),NULL,NULL);
 INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Revistas importadas','Distribución de revistas importadas.',CURRENT_DATE(),NULL,NULL);
 INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Chucherías nacionales','Distribución de chucherías nacionales.',CURRENT_DATE(),NULL,NULL);
 INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Chucherías importadas','Distribución de chucherías importadas.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Periódicos','Distribución de periódicos.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Albumes y barajitas','Distribución de albumes y barajitas.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Juguetes','Distribución de juguetes, muñecas, carros, trompos, perinolas, etc.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Misceláneos','Distribución de accesorios para cualquier tipo de cosa, hilos, agujas, ligas, bolsas de regalo y normales, globos, etc.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Baterías','Distribución de pilas.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Papelería','Distribución de lápices, borras, minas, teipe, grapas, hojas, etc.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Cigarros y/o Tabacos','Distribución de cigarros y/o tabacos nacionales e importados.',CURRENT_DATE(),NULL,NULL);
+INSERT INTO KIOSCO.TIPO_SERVICIOS VALUES (DEFAULT,'Lotería','Distribución de juegos de loteria: Kino, Triple Gordo, Lotto Florida, etc.',CURRENT_DATE(),NULL,NULL);
